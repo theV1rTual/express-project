69f5e4cd0b61ab3a7515e53b0d27518b6fd2ce92
theV1rTual <arystandev@gmail.com>
2025-11-14 20:54:42 +0500

    FEATURE: fixed validation

--- src/blogs/validation/blog.input-dto.validation-middlewares.ts
+++ src/blogs/validation/blog.input-dto.validation-middlewares.ts
@@ -8,14 +8,18 @@ const nameValidation = body('name')
   .withMessage('Name should be string')
   .trim()
   .isLength({ max: 15 })
-  .withMessage('Length of name cannot be more than 15');
+  .withMessage('Length of name cannot be more than 15')
+  .notEmpty()
+  .withMessage('Name cannot be empty');
 
 const descriptionValidation = body('description')
   .isString()
   .withMessage('Description should be string')
   .trim()
   .isLength({ max: 500 })
-  .withMessage('Length of description cannot be more that 500');
+  .withMessage('Length of description cannot be more that 500')
+  .notEmpty()
+  .withMessage('Description cannot be empty');
 
 const websiteUrlValidation = body('websiteUrl')
   .isString()
@@ -24,7 +28,9 @@ const websiteUrlValidation = body('websiteUrl')
   .isLength({ max: 100 })
   .withMessage('Length of websiteUrl cannot be more than 100')
   .matches(WEBSITE_RE)
-  .withMessage('websiteUrl should be a valid https URL');
+  .withMessage('websiteUrl should be a valid https URL')
+  .notEmpty()
+  .withMessage('websiteUrl cannot be empty');
 
 export const blogCreateInputValidation = [
   nameValidation,
