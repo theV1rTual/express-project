017c0a79ce1c412218b128d16fee0605d153eb9f
theV1rTual <arystandev@gmail.com>
2025-12-02 16:49:12 +0500

    FEATURE: fixed create post method

--- src/posts/routers/handlers/create-post.handler.ts
+++ src/posts/routers/handlers/create-post.handler.ts
@@ -14,6 +14,6 @@ export async function createPostHandler(
     blogId: req.body.blogId,
   };
 
-  await postsRepository.create(newPost);
-  res.status(HttpStatuses.CREATED).send(newPost);
+  const createdPost = await postsRepository.create(newPost);
+  res.status(HttpStatuses.CREATED).send(createdPost);
 }
