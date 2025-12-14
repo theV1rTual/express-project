6e2249bdce086be9302f478b7279f957f1498f26
theV1rTual <arystandev@gmail.com>
2025-12-14 17:02:24 +0500

    FEATURE: fixed createPost handler

--- src/blogs/routers/handlers/create-post-for-blog.handler.ts
+++ src/blogs/routers/handlers/create-post-for-blog.handler.ts
@@ -2,14 +2,24 @@ import { Request, Response } from 'express';
 import { PostInputDto } from '../../../posts/dto/post.input-dto';
 import { postsRepository } from '../../../posts/repositories/posts.repository';
 import { HttpStatuses } from '../../../core/types/http-statuses';
+import { ObjectId } from 'mongodb';
+
+type Params = { id: string };
 
 export async function createPostForBlogHandler(
-  req: Request<{}, {}, Omit<PostInputDto, 'blogId'>>,
+  req: Request<Params, {}, Omit<PostInputDto, 'blogId'>>,
   res: Response,
 ) {
+  const blogId = req.params.id;
+
+  if (!ObjectId.isValid(blogId)) {
+    res.sendStatus(HttpStatuses.NOT_FOUND);
+    return;
+  }
+
   const newPost: PostInputDto = {
     title: req.body.title,
-    blogId: req.query.id as string,
+    blogId,
     content: req.body.content,
     shortDescription: req.body.shortDescription,
   };
