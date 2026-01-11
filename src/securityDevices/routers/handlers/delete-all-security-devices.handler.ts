f6bf6ac8ec04432266f0aea414bdb7622ff23975
theV1rTual <arystandev@gmail.com>
2026-01-11 20:41:46 +0500

    FEATURE: fixes

--- src/securityDevices/routers/handlers/delete-all-security-devices.handler.ts
+++ src/securityDevices/routers/handlers/delete-all-security-devices.handler.ts
@@ -2,19 +2,21 @@ import { Request, Response } from 'express';
 import { jwtService } from '../../../auth/adapters/jwt.service';
 import { HttpStatuses } from '../../../core/types/http-statuses';
 import { securityDevicesCollection } from '../../../db/mongo.db';
+import { ObjectId } from 'mongodb';
 
 export async function deleteAllSecurityDevicesHandler(
   req: Request,
   res: Response,
 ) {
   const refreshToken = req.cookies.refreshToken;
+  const payload = await jwtService.verifyRefreshToken(refreshToken);
 
-  if (!(await jwtService.verifyRefreshToken(refreshToken.refreshToken))) {
+  if (!payload) {
     return res.sendStatus(HttpStatuses.UNAUTHORIZED);
   }
 
   const result = await securityDevicesCollection.deleteMany({
-    userId: refreshToken.userId,
+    userId: new ObjectId(payload?.userId),
   });
 
   if (result) {
