8acbdd73c15aa335daf8b5558210dd14d791aeb4
theV1rTual <arystandev@gmail.com>
2026-01-14 17:32:22 +0500

    FEATURE: fixes

--- src/securityDevices/routers/handlers/get-security-handlers.handler.ts
+++ src/securityDevices/routers/handlers/get-security-handlers.handler.ts
@@ -1,31 +1,28 @@
 import { Request, Response } from 'express';
-import {
-  refreshTokensCollection,
-  securityDevicesCollection,
-} from '../../../db/mongo.db';
+import { securityDevicesCollection } from '../../../db/mongo.db';
 import { HttpStatuses } from '../../../core/types/http-statuses';
 import { mapSecurityDevicesDbToView } from '../mappers/mapSecurityDevices';
 import { jwtService } from '../../../auth/adapters/jwt.service';
 import { ObjectId } from 'mongodb';
 
 export async function getSecurityDevicesHandler(req: Request, res: Response) {
-  const refreshToken = req.cookies.refreshToken;
-  if (!refreshToken) return res.sendStatus(HttpStatuses.UNAUTHORIZED);
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    const payload = await jwtService.verifyRefreshToken(refreshToken);
 
-  const payload = await jwtService.verifyRefreshToken(refreshToken);
-  if (!payload) return res.sendStatus(HttpStatuses.UNAUTHORIZED);
+    if (!(await jwtService.verifyRefreshToken(refreshToken))) {
+      return res.sendStatus(HttpStatuses.UNAUTHORIZED);
+    }
+    const result = await securityDevicesCollection
+      .find({ userId: new ObjectId(payload?.userId) })
+      .toArray();
+    if (!result) {
+      return res.sendStatus(HttpStatuses.NOT_FOUND);
+    }
 
-  const tokenDoc = await refreshTokensCollection.findOne({
-    value: refreshToken,
-  });
-  if (!tokenDoc || !tokenDoc.isValid) {
-    return res.sendStatus(HttpStatuses.UNAUTHORIZED);
+    const view = result.map(mapSecurityDevicesDbToView);
+    return res.status(HttpStatuses.OK).send(view);
+  } catch (e) {
+    return res.sendStatus(HttpStatuses.INTERNAL_SERVER_ERROR);
   }
-
-  const result = await securityDevicesCollection
-    .find({ userId: new ObjectId(payload.userId) })
-    .toArray();
-
-  const view = result.map(mapSecurityDevicesDbToView);
-  return res.status(HttpStatuses.OK).send(view);
 }
